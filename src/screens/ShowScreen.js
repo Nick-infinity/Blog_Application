91f97ca3e334bfe35d4794b044999d8299d918f9
Nickinfinity <nikhilg.me@gmail.com>
2021-04-25 13:54:51 +0530

    ShowScreen: Add functionality to display data from state using context

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { Context } from '../context/BlogContext';
 const ShowScreen = ({ route }) => {
 	const { id } = route.params;
 
+	const { state } = useContext(Context);
+	const blogPostWithId = state.find((blogPost) => blogPost.id === id);
+
 	return (
 		<View>
-			<Text>ShowScreen</Text>
+			<Text>{blogPostWithId.title}</Text>
 		</View>
 	);
 };
