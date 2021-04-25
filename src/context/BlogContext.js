2068b1a0c0de35de6eb92539d7f897d0a9e9bc94
Nickinfinity <nikhilg.me@gmail.com>
2021-04-25 08:49:13 +0530

    Getting array of objects from our BlogProvider

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,14 @@ import { Children } from 'react/cjs/react.development';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-	return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+	const blogPosts = [
+		{ title: 'BlogPost 1' },
+		{ title: 'BlogPost 2' },
+		{ title: 'BlogPost 3' },
+	];
+	return (
+		<BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+	);
 };
 
 export default BlogContext;
