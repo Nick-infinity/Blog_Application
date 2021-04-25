595dfae0bf19c05fb1af2a00fe0c0cf6c387468f
Nickinfinity <nikhilg.me@gmail.com>
2021-04-25 08:10:44 +0530

    App:Add Index Screen and Satck Navigation

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import IndexScreen from './src/screens/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Index">
+				<Stack.Screen
+					name="Index"
+					component={IndexScreen}
+					options={{ title: 'Blog Index', headerTitleAlign: 'center' }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
