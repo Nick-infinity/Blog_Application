9b3e62d055f81adbe878ec10f58bee78b2730c4f
Nickinfinity <nikhilg.me@gmail.com>
2021-04-25 15:00:45 +0530

    App: Add EditScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,20 @@
+import React, { useContextt } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	TextInput,
+} from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const EditScreen = ({ route, navigation }) => {
+	return (
+		<View>
+			<Text>EditScreen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+export default EditScreen;
